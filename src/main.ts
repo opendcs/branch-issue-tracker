3e4f48dcb01391b388c3d261579cbb47ddc936de
Mike Neilson <michael.a.neilson@usace.army.mil>
2025-06-27 16:14:49 +0000

    Initial test data and setup.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
+import { graphql } from "@octokit/graphql";
+import { Repository } from "@octokit/graphql-schema";
 
+import { Context } from '@actions/github/lib/context.js'
 /**
  * The main function for the action.
  *
@@ -7,15 +11,31 @@ import * as core from '@actions/core'
  */
 export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const gh: Context  = github.context
+    const octokit = github.getOctokit(core.getInput("token"))
+    const base_issue = gh.payload.issue
+    console.log(base_issue)
+    
+    const {repository} = await octokit.graphql<{repository: Repository}>(
+    {query:`query lastIssues($owner: String!, $repo: String!) {
+  repository(owner:$owner, name:$repo) {
+    issues(last:3) {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+}`, 
+  owner: gh.repo.owner,
+  repo: gh.repo.repo
+    }
+    )
+    console.log(repository.issues.edges)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-
-    core.debug(new Date().toTimeString())
+    
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
